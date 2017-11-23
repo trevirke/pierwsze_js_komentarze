58424f92b0ace4a834700ee6dc6bd32dd986dfdb
Sylwia Mamczur <sylwia.mamczur@gmail.com>
2017-11-23 13:27:44 +0100

    Change files

---
+++ script.js
@@ -0,0 +1,11 @@
+//js Ćwiczenia - komentowanie
+
+/* 
+to jest komentarz blokowy,
+można zawrzeć w nim wiele linii.
+*/
+
+var name = prompt('Sylwia');
+//funkcja allert wyświetla wartość w małym okienku
+alert('Hello, ' + name);
+console.log('Hello, ' + name);
